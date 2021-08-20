94cbbc0aff8df8f13143f23cedc6a409cd5dc003
Xieyufengdd <70199143+Xieyufengdd@users.noreply.github.com>
2021-08-21 00:27:34 +0800

    Add files via upload
    update reg and log part

--- src/main/java/com/example/tinymall/service/UserService.java
+++ src/main/java/com/example/tinymall/service/UserService.java
@@ -5,9 +5,11 @@ import com.example.tinymall.entity.Users;
 public interface UserService {
     public Users selectUserByUsername(String name);
 
+    public Users selectUserByEmail(String email);
+
     public void registerNewUser(Users user);
 
-    public Users checkUserPassword(String username, String password);
+    public Users checkUserPassword(String email, String password);
 
     public void updateUserInformation(Users user);
 
